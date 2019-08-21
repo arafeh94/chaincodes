45203b8fbf1588f3a53e7af3064c6ec32fb472b0
arafeh <arafeh198@gmail.com>
2019-08-21 02:12:54 -0700

    init

---
+++ src/chaincode-reference.js
@@ -0,0 +1,198 @@
+const StorageChaincode = require('chaincode-node-storage');
+const ExpectationMaximization = require('ml-expectation-maximization').ExpectationMaximization;
+const shim = require('fabric-shim');
+const tools = require('chaincode-node-storage/chaincode-tools');
+
+
+module.exports = class ReferenceChaincode extends StorageChaincode {
+    /**
+     * ===========================================================================
+     * Code bellow belong to cctaskpts/taskpts {chaincode/channel}
+     * ===========================================================================
+     */
+
+    /**
+     * data to fit for expectation maximisation are 2d array x=>user, y=>feature.
+     * feature present the count of each activity
+     * @param args
+     * @returns {Promise<string>}
+     */
+    async quality(args) {
+        if (args.length < 1) {
+            throw new Error('incorrect number of arguments, taskId at pos 0 is required');
+        }
+        let tid = args[0];
+        let exists = await this.getTaskpts([tid]).then(value => JSON.parse(value));
+        if (exists && exists.length > 0) {
+            throw new Error("quality if task {" + tid + "} already calculated");
+        } else {
+            let data = await super.invokeChaincode('ccobservations', ['aggregate', tid], 'observations').then(value => JSON.parse(value));
+            if (!data || tools.sizeOf(data) == 0) {
+                throw new Error('no observations, please ensure you input the correct task id');
+            } else {
+                let training = [];
+                for (let uid in data) {
+                    training.push(data[uid]);
+                }
+                const em = new ExpectationMaximization({numClusters: 1});
+                em.train(training);
+                //TODO: need a method to calculate the scores
+                let scores = em.predict(training);
+                for (let uid in data) {
+                    let taskpts = this.createTaskpts(tid, uid, 10, 10);
+                    super.put(['taskpts', tools.uuid(), taskpts])
+                }
+                return "done";
+            }
+        }
+    }
+
+    /**
+     * [1] return all the taskpts of task 1
+     * [1,1] return all the taskpts of user 1 in task 1
+     * [false,1] return all the taskpts of user 1 in all tasks
+     *
+     * @param args
+     * @returns {Promise<Buffer>}
+     */
+    async getTaskpts(args) {
+        let tid = args[0] || false;
+        let uid = args[1] || false;
+        return super.list(['taskpts'])
+            .then(value => JSON.parse(value))
+            .then(value => value.filter(item => {
+                let accept = true;
+                if (tid !== false) {
+                    accept = item.value.tid == tid;
+                }
+                if (accept && uid !== false) {
+                    accept = item.value.uid == uid;
+                }
+                return accept;
+            })).then(value => Buffer.from(JSON.stringify(value)))
+    }
+
+    createTaskpts($taskId, $userId, $quality, $payment) {
+        return JSON.stringify({
+            'tid': $taskId,
+            'uid': $userId,
+            'quality': $quality,
+            'payment': $payment,
+            'isDeleted': 0
+        });
+    }
+
+
+    /**
+     * ===========================================================================
+     * Code bellow belong to ccobservations/observations {chaincode/channel}
+     * ===========================================================================
+     */
+    /**
+     * [1] return all the observations of task 1
+     * [1,1] return all the observations of user 1 in task 1
+     * [false,1] return all the observations of user 1 in all tasks
+     * @param args
+     * @returns {Promise<Buffer>}
+     */
+    async observations(args) {
+        let tid = args[0] || false;
+        let uid = args[1] || false;
+        return super.list(['observation'])
+            .then(value => JSON.parse(value))
+            .then(value => value.filter(obs => {
+                let accept = obs.isDeleted || true;
+                if (tid !== false) {
+                    accept = obs.value.tid == tid;
+                }
+                if (accept && uid !== false) {
+                    accept = obs.value.uid == uid;
+                }
+                return accept;
+            }))
+            .then(value => Buffer.from(JSON.stringify(value)));
+    }
+
+    async aggregate(args) {
+        let tid = args[0] || false;
+        let uid = args[1] || false;
+        let observations = await super.invokeChaincode('ccobservations', ['observations', tid, uid], 'observations').then(value => JSON.parse(value));
+        if (!observations) {
+            return Buffer.from(JSON.stringify([]));
+        } else {
+            let data = {};
+            for (let pos in observations) {
+                let observation = observations[pos].value;
+                let uid = observation.uid.toString();
+                let record = JSON.parse(observation.record);
+                if (!data[uid]) data[uid] = [];
+                data[uid].push(parseInt(record.activity));
+            }
+            let agg = {};
+            for (let uid in data) {
+                let activities = data[uid];
+                let set = [];
+                [1, 2, 3, 4, 5, 6, 7].forEach(value => {
+                    set[value] = activities.filter(act => {
+                        return act == value
+                    }).length;
+                });
+                set = set.filter(value => value != null || value != undefined);
+                agg[uid] = set;
+            }
+            return Buffer.from(JSON.stringify(agg));
+        }
+    }
+
+    /**
+     * ===========================================================================
+     * Code bellow belong to ccreports/reports {chaincode/channel}
+     * ===========================================================================
+     */
+
+    /**
+     * [1] generate (optional) and return the report of task 1
+     * @param args
+     * @returns {Promise<Buffer>}
+     */
+    async report(args) {
+        if (args.length < 1) {
+            throw new Error('incorrect number of arguments, taskId at pos 0 is required');
+        }
+        let report = await super.list(['reports']).then(value => JSON.parse(value));
+        if (!report || report.length == 0) {
+            let tid = args[0];
+            let data = await super.invokeChaincode('ccobservations', ['aggregate', tid], 'observations').then(value => JSON.parse(value));
+            report = {tid: tid, data: {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0}};
+            for (let uid in data) {
+                for (let activity in data[uid]) {
+                    report.data[parseInt(activity) + 1] += data[uid][activity];
+                }
+            }
+            let jsonReport = JSON.stringify(report);
+            super.put(['reports', jsonReport]);
+            return Buffer.from(jsonReport);
+        } else {
+            return Buffer.from(JSON.stringify(report));
+        }
+    }
+
+    /**
+     * check if the report is generated or not
+     * @param args
+     * @returns {Promise<Buffer>}
+     */
+    async exists(args) {
+        if (args.length < 1) {
+            throw new Error('incorrect number of arguments, taskId at pos 0 is required');
+        }
+        let tid = args[0];
+        let response = await super.list(['reports']).then(value => JSON.parse(value));
+        if (!response || response.length == 0) {
+            return Buffer.from(JSON.stringify(false));
+        } else {
+            return Buffer.from(JSON.stringify(true));
+        }
+    }
+
+};
---
+++ src/test.js
@@ -0,0 +1,207 @@
+const tools = require('chaincode-node-storage/chaincode-tools');
+const shim = require('fabric-shim');
+const Chaincode = require('./chaincode-reference');
+const mock = require("@theledger/fabric-mock-stub");
+
+async function main() {
+    await new Promise(async function () {
+        const ccuser = new mock.ChaincodeMockStub("ccuser", new Chaincode());
+        const cctask = new mock.ChaincodeMockStub("cctask", new Chaincode());
+        const ccobservations = new mock.ChaincodeMockStub("ccobservations", new Chaincode());
+        const cctaskpts = new mock.ChaincodeMockStub("cctaskpts", new Chaincode());
+        const ccreputation = new mock.ChaincodeMockStub("cctaskp", new Chaincode());
+        const ccreport = new mock.ChaincodeMockStub("ccreport", new Chaincode());
+        ccuser.mockPeerChaincode('src/users', ccuser);
+        ccuser.mockPeerChaincode('cctasks/tasks', cctask);
+        ccuser.mockPeerChaincode('ccobservations/observations', ccobservations);
+        ccuser.mockPeerChaincode('cctaskpts/taskpts', cctaskpts);
+        ccuser.mockPeerChaincode('ccreputations/reputations', ccreputation);
+        ccuser.mockPeerChaincode('ccreports/reports', ccreport);
+
+        cctask.mockPeerChaincode('src/users', ccuser);
+        cctask.mockPeerChaincode('cctasks/tasks', cctask);
+        cctask.mockPeerChaincode('ccobservations/observations', ccobservations);
+        cctask.mockPeerChaincode('cctaskpts/taskpts', cctaskpts);
+        cctask.mockPeerChaincode('ccreputations/reputations', ccreputation);
+        cctask.mockPeerChaincode('ccreports/reports', ccreport);
+
+        ccobservations.mockPeerChaincode('src/users', ccuser);
+        ccobservations.mockPeerChaincode('cctasks/tasks', cctask);
+        ccobservations.mockPeerChaincode('ccobservations/observations', ccobservations);
+        ccobservations.mockPeerChaincode('cctaskpts/taskpts', cctaskpts);
+        ccobservations.mockPeerChaincode('ccreputations/reputations', ccreputation);
+        ccobservations.mockPeerChaincode('ccreports/reports', ccreport);
+
+        cctaskpts.mockPeerChaincode('src/users', ccuser);
+        cctaskpts.mockPeerChaincode('cctasks/tasks', cctask);
+        cctaskpts.mockPeerChaincode('ccobservations/observations', ccobservations);
+        cctaskpts.mockPeerChaincode('cctaskpts/taskpts', cctaskpts);
+        cctaskpts.mockPeerChaincode('ccreputations/reputations', ccreputation);
+        cctaskpts.mockPeerChaincode('ccreports/reports', ccreport);
+
+        ccreputation.mockPeerChaincode('src/users', ccuser);
+        ccreputation.mockPeerChaincode('cctasks/tasks', cctask);
+        ccreputation.mockPeerChaincode('ccobservations/observations', ccobservations);
+        ccreputation.mockPeerChaincode('cctaskpts/taskpts', cctaskpts);
+        ccreputation.mockPeerChaincode('ccreputations/reputations', ccreputation);
+        ccreputation.mockPeerChaincode('ccreports/reports', ccreport);
+
+        ccreport.mockPeerChaincode('src/users', ccuser);
+        ccreport.mockPeerChaincode('cctasks/tasks', cctask);
+        ccreport.mockPeerChaincode('ccobservations/observations', ccobservations);
+        ccreport.mockPeerChaincode('cctaskpts/taskpts', cctaskpts);
+        ccreport.mockPeerChaincode('ccreputations/reputations', ccreputation);
+        ccreport.mockPeerChaincode('ccreports/reports', ccreport);
+
+        await ccuser.mockInit("tx1", []);
+        await cctask.mockInit("tx1", []);
+        await ccobservations.mockInit("tx1", []);
+        await cctaskpts.mockInit("tx1", []);
+        await ccreputation.mockInit("tx1", []);
+        await ccreport.mockInit("tx1", []);
+
+        await test2(ccuser, cctask, ccobservations, cctaskpts, ccreputation, ccreport);
+
+    });
+
+
+    /**
+     * @param {ChaincodeMockStub} ccuser
+     * @param {ChaincodeMockStub} cctask
+     * @param {ChaincodeMockStub} ccobservations
+     * @param {ChaincodeMockStub} cctaskpts
+     * @param {ChaincodeMockStub} ccreputation
+     * @param {ChaincodeMockStub} ccreport
+     */
+    async function test1(ccuser, cctask, ccobservations, cctaskpts, ccreputation, ccreport) {
+        let res = false;
+
+        res = await cctask.mockInvoke("tx2", ['put', 'task', '1', user('arafeh', 'arafeh198', '123456', 'c')]);
+        res = await cctask.mockInvoke("tx2", ['list', 'task']);
+        sts(res);
+        res = await cctask.mockInvoke("tx2", ['t']);
+        console.log(res);
+        sts(res)
+    }
+
+    /**
+     * @param {ChaincodeMockStub} ccuser
+     * @param {ChaincodeMockStub} cctask
+     * @param {ChaincodeMockStub} ccobservations
+     * @param {ChaincodeMockStub} cctaskpts
+     * @param {ChaincodeMockStub} ccreputation
+     * @param {ChaincodeMockStub} ccreport
+     */
+    async function test2(ccuser, cctask, ccobservations, cctaskpts, ccreputation, ccreport) {
+        let res = false;
+
+        res = await ccuser.mockInvoke("tx2", ['put', 'task', '1', user('arafeh', 'arafeh198', '123456', 'c')]);
+
+        res = await ccuser.mockInvoke("tx2", ['put', 'user', '1', user('arafeh', 'arafeh198', '123456', 'c')]);
+        res = await ccuser.mockInvoke("tx2", ['put', 'user', '2', user('hammoud', 'hammoud123', '123456', 'p')]);
+        res = await cctask.mockInvoke("tx2", ['put', 'task', '1', task(1, 'collect', 10, 100)]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(1, 1, {activity: '4'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(1, 1, {activity: '4'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(1, 1, {activity: '4'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(1, 1, {activity: '4'})]);
+
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(2, 1, {activity: '1'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(2, 1, {activity: '1'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(2, 1, {activity: '1'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(2, 1, {activity: '1'})]);
+
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(3, 1, {activity: '4'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(3, 1, {activity: '4'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(3, 1, {activity: '2'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(3, 1, {activity: '2'})]);
+
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(4, 1, {activity: '4'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(4, 1, {activity: '4'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(4, 1, {activity: '3'})]);
+        res = await ccobservations.mockInvoke("tx2", ['put', 'observation', atomic(), observation(4, 1, {activity: '1'})]);
+
+        console.clear();
+        res = await cctaskpts.mockInvoke("tx3", ['quality', '1']);
+        res = await cctaskpts.mockInvoke("tx3", ['list', 'taskpts']);
+        res = await ccreport.mockInvoke("tx3", ['report', '1']);
+        sts(res);
+
+    }
+
+}
+
+function sts(res) {
+    console.log(res.status);
+    console.log(res.payload.toString());
+}
+
+function dis(res) {
+    let ar = parse(res.payload);
+    for (let pos in ar) {
+        let item = ar[pos];
+        console.log(item.key, ':', json(item.value))
+    }
+}
+
+function json(obj) {
+    return JSON.stringify(obj)
+}
+
+function parse(obj) {
+    return JSON.parse(obj)
+}
+
+function user(name, email, password, type) {
+    return json({
+        'username': name,
+        'email': email,
+        'password': password,
+        'type': type,
+        'isDeleted': 0
+    });
+}
+
+function task($userId, $title, $minQuality, $budget) {
+    return json({
+        'minQ': $minQuality,
+        'budget': $budget,
+        'expDt': '',
+        'uid': $userId,
+        'title': $title,
+        'status': 'PENDING',
+    });
+}
+
+function observation($userId, $taskId, $record) {
+    return json({
+        'uid': $userId,
+        'tid': $taskId,
+        'date': new Date(),
+        'record': JSON.stringify($record),
+        'isDeleted': 0
+    });
+}
+
+function taskQ($taskId, $userId) {
+    return json({
+        'tid': $taskId,
+        'uid': $userId,
+        'quality': 0,
+        'payment': 0,
+        'isDeleted': 0
+    });
+}
+
+function panic(error) {
+    console.error(error);
+    process.exit(1);
+}
+
+i = 0;
+
+function atomic() {
+    return (i++).toString();
+}
+
+// https://stackoverflow.com/a/46916601/1478566
+main().catch(panic).then(clearInterval.bind(null, setInterval(a => a, 1E9)));
